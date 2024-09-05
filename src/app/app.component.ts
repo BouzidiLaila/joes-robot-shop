fdcf702907b35c2e5ca1ee3cd96266dfcc92cf6e
Laila Chabab <chabablaila@gmail.com>
2024-09-05 18:23:44 +0200

    message de commit "chapter4"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { CatalogComponent } from "./catalog/catalog.component";
+import { SiteHeaderComponent } from './site-header/site-header.component';
+
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SiteHeaderComponent,CatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'joes-robot-shop-laila';
+ 
 }
